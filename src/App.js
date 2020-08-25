4a6dd5d2bb774408baa97bfca3d83b46d7ec7443
Akshaytamrakar <akshay.tamrakar55@gmail.com>
2020-08-26 02:27:55 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>React Router Example</title>
-  </head>
-  <body>
-    <div id="root"></div>
-  </body>
-</html>
+import React, { Component } from "react";
+ 
+class Main extends Component {
+  render() {
+    return (
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><a href="/">Home</a></li>
+            <li><a href="/stuff">Stuff</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+          <div className="content">
+             
+          </div>
+        </div>
+    );
+  }
+}
+ 
+export default Main;
