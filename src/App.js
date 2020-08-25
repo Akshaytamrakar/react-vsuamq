0b51c9611fd315ed2d26e12f1220a2d3bc9b7437
Akshaytamrakar <akshay.tamrakar55@gmail.com>
2020-08-26 02:24:40 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
-class Login extends Component {
-constructor(props){
-  super(props);
-  this.state={
-  username:'',
-  password:''
-  }
- }
-render() {
-    return (
-      <div>
-        <MuiThemeProvider>
-          <div>
-          <AppBar
-             title="Login"
-           />
-           <TextField
-             hintText="Enter your Username"
-             floatingLabelText="Username"
-             onChange = {(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
-               onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-         </div>
-         </MuiThemeProvider>
-      </div>
-    );
-  }
-}
-const style = {
- margin: 15,
-};
-export default Login;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>React Router Example</title>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import Main from "./Main";
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
